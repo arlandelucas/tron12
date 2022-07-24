7f675a349a2e9ef36a4339fbb1f65311c6fc80a0
arlandelucas64@gmail.com <arlandelucas64@gmail.com>
2022-07-24 22:16:32 +0100

    uy

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 
     $dbHost = 'localhost';
-    $dbUsername = 't94veo1r_Arlan';
-    $dbPassword = '4O;YPx1%fBXU';
+    $dbUsername = 'root';
+    $dbPassword = '';
     $dbName = 't94veo1r_admin';
     
     $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
---
+++ usuarios.sql
@@ -0,0 +1,76 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.7
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:
+-- Tempo de geração: 24/07/2022 às 23:12
+-- Versão do servidor: 5.7.36-cll-lve
+-- Versão do PHP: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `t94veo1r_admin`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nome` text NOT NULL,
+  `senha` text NOT NULL,
+  `email` text NOT NULL,
+  `telefone` text NOT NULL,
+  `genero` text NOT NULL,
+  `data_nascimento` text NOT NULL,
+  `cidade` text NOT NULL,
+  `estado` text NOT NULL,
+  `endereco` text NOT NULL,
+  `carteira` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Despejando dados para a tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nome`, `senha`, `email`, `telefone`, `genero`, `data_nascimento`, `cidade`, `estado`, `endereco`, `carteira`) VALUES
+(1, '', '123', 'arlandelucas6@gmail.com', '', '', '', '', '', '', 0),
+(2, '', '123', 'arlandelucas6@gmail.com', '', '', '', '', '', '', 0);
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
